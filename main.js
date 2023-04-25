bd7b3a68202d469f80aec38ab260c251cfdfcda7
Kosternoy Dmitriy Olegovich <130212889+AriohDeKos@users.noreply.github.com>
2023-04-25 12:22:46 +0300

    l1

---
+++ main.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $('.slider').bxSlider({
+        auto: true,
+        startAuto:true,
+        pager: false,
+        slideWidth: 1000,
+        touchEnabled: true,
+        shrinkItems:false,
+        controls: false,
+        autoControlsSelector:null,
+        pause: 3000,
+    });
+  }); 
